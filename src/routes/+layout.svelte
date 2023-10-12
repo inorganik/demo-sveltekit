72fbf47d79ceb58ec5e103f6195e57fd79ac45ce
Jamie Perkins <jamie.perkins@gmail.com>
2023-10-12 06:04:27 -0600

    transition demo, add tailwind typography

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,9 @@
 <script>
   import "../app.css";
   import hash from '$lib/state/hash.js'
+  import { fly } from 'svelte/transition';
 
+  let step = 1
   let myDialog
 
   $: if ($hash === '#login') {
@@ -17,6 +19,12 @@
   function clearHash() {
     window.location.hash = '';
   }
+  function changeStep(amount) {
+    step += amount;
+    if (step < 1) step = 1;
+    if (step > 2) step = 2;
+  }
+
 </script>
 
 <svelte:head>
@@ -26,12 +34,11 @@
 <div class="mb-6 mx-auto px-2 md:px-4 max-w-5xl relative flex flex-col min-h-full">
   <div class="navbar bg-base-100 my-6 p-0">
     <div class="flex-1">
-      <h1 class="text-5xl font-bold">Svelte Demo</h1>
+      <h1 class="text-5xl font-bold"><a href="/">Welcome to Svelte!</a></h1>
     </div>
     <div class="flex-none">
       <ul class="menu menu-horizontal px-1">
         <li><a href="#login">open modal</a></li>
-        <li><a href="/count-up">count up</a></li>
       </ul>
     </div>
   </div>
@@ -40,11 +47,30 @@
 </div>
 
 <dialog bind:this={myDialog} on:close={handleClose} class="modal">
-  <div class="modal-box bg-white">
-    <h3 class="font-bold text-lg">Hello!</h3>
-    <form method="dialog">
-      <button class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
-    </form>
-    <p class="py-4">Here is a modal. You can press ESC to close</p>
+  <div class="modal-box max-w-3xl bg-white p-5">
+    <button class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2 z-10" on:click={handleClose}>✕</button>
+
+    <div class="min-h-[500px] relative overflow-hidden prose">
+      {#if step === 1}
+        <div transition:fly={{ duration: 500, x: -768 }} class="rounded-lg overflow-hidden w-full min-h-[500px] absolute">
+          <h1>Step 1</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum malesuada, massa eu sodales elementum, arcu enim imperdiet ligula, nec lobortis odio tortor id magna. Mauris dapibus interdum augue, dictum pharetra neque malesuada non. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nulla dapibus et justo non faucibus.</p>
+          <p>Morbi gravida, leo eget fermentum faucibus, ante magna molestie arcu, nec varius diam urna vel dui. Nunc venenatis, ex et dignissim consequat, risus purus malesuada tortor, quis lobortis odio orci eget quam. Mauris sem nibh, placerat elementum suscipit vel, aliquam sed arcu. Pellentesque efficitur dui nibh, non sagittis nisi aliquet non. In eget egestas risus. Morbi et egestas neque, vitae dignissim velit.</p>
+        </div>
+
+      {:else if step === 2}
+        <div transition:fly={{ duration: 500, x: 768 }} class="rounded-lg overflow-hidden w-full min-h-[500px] absolute">
+          <h1>Step 2</h1>
+          <p>Etiam pulvinar, nisl ac pharetra imperdiet, ante nunc bibendum ligula, tristique hendrerit lectus diam quis nibh. Quisque nisl libero, tempor sit amet erat a, aliquam rhoncus neque. Aenean eget velit a mi faucibus ultricies elementum sed purus. Maecenas bibendum faucibus sem, bibendum efficitur purus.</p>
+          <p>Fusce vel rutrum lorem, eu pretium arcu. Praesent pretium augue odio, quis sagittis est auctor non. Phasellus ac justo leo. Vestibulum vestibulum finibus purus, ornare accumsan ex vulputate non. Nullam molestie risus quis faucibus rhoncus. Etiam porttitor velit pulvinar maximus ultricies. Nam faucibus sem vel tortor sagittis blandit. Mauris ultricies nisi in libero commodo, sed dictum tortor rhoncus. Suspendisse feugiat aliquam nisi. Phasellus finibus aliquet lobortis. Vestibulum in eros quis ex fringilla accumsan non vel velit.</p>
+        </div>
+
+      {/if}
+    </div>
+    <div class="modal-action mt-8">
+      <button type="button" class="btn btn-ghost" disabled={step === 1} on:click={() => changeStep(-1)}>Back</button>
+      <span class="flex-grow"></span>
+      <button type="button" class="btn btn-primary" disabled={step === 2} on:click={() => changeStep(1)}>Next</button>
+    </div>
   </div>
 </dialog>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,19 @@
-<h1>Home</h1>
+<script>
+  import { tweened } from 'svelte/motion'
+  import { quintOut } from 'svelte/easing'
+  import { derived } from 'svelte/store'
+  
+  let myNumber = tweened(0, { duration: 2000, easing: quintOut })
+  let formatted = derived(myNumber, ($myNumber) => $myNumber.toFixed())
+</script>
+
+<svelte:head>
+  <title>Count demo</title>
+</svelte:head>
+
+<p>Count demo</p>
+<h1 class="text-6xl font-bold my-24">{$formatted}</h1>
+<div>
+  <button class="btn btn-secondary mt-5 mr-2" on:click={() => myNumber.set(2500)}>Count!</button>
+  <button type="button" class="btn btn-ghost !font-normal" value="cancel" on:click={() => myNumber.set(0, { duration: 0 })}>Reset</button>
+</div>
